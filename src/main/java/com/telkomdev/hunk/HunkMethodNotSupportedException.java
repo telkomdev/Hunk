4d30992d1dc25912877f7b89c789633fb1488360
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-04-03 12:35:02 +0700

    add Hunk method not supported exception

--- src/main/java/com/telkomdev/hunk/HunkMethodNotSupportedException.java
+++ src/main/java/com/telkomdev/hunk/HunkMethodNotSupportedException.java
@@ -1,4 +1,12 @@
 package com.telkomdev.hunk;
 
-public class HunkMethodNotSupportedException {
+public class HunkMethodNotSupportedException extends Exception {
+
+    public HunkMethodNotSupportedException(String method) {
+        super(method + " http method is not supported");
+    }
+
+    public HunkMethodNotSupportedException(String method, Throwable err) {
+        super(method + " http method is not supported", err);
+    }
 }
